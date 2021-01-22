b7fb8e253e915d39cf192e28237e55ec4b43288b
zhujinrun <15997352948@163.com>
2021-01-22 10:08:47 +0800

    添加项目文件。

---
+++ HelloAbp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloAbp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
